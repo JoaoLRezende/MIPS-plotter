df7981670312d6917e7377cbd7c955d578b04706
João Rezende <30845402+jplr98@users.noreply.github.com>
2018-07-15 20:02:03 -0300

    Remove unnecessary comments

--- global_constants.asm
+++ global_constants.asm
@@ -1,13 +1,10 @@
-	# The screen's width and height are measured by the Bitmap Display tool in display "units"; they're equal
-	# to the number of pixels divided by the number pixels per unit.
+	# The screen's width and height are measured by the Bitmap Display tool in display units;
+	# their number is equal to the total number of pixels divided by the number of pixels per unit.
+	# Each display unit corresponds to one cartesian-plane point of integral coordinates.
 	.eqv	DISPLAY_WIDTH		256	# = 512 / 2
 	.eqv	DISPLAY_HEIGHT		128	# = 256 / 2
 	# The framebuffer's size (in bytes) must then be 4 * DISPLAY_WIDTH * DISPLAY_HEIGHT.
 	.eqv	TOTAL_FRAMEBUFFER_SIZE	131072	# = 4 * 256 *128
-	# This program makes no assumptions about the size of the screen, and should accept
-	# viewports of any reasonable size. Each display unit corresponds to one cartesian-plane
-	# point of integer coordinates.
-
 
 
 	.eqv  BG_COLOR     0xFFFFFF  # white
