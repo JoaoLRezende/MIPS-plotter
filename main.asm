075ce284c4f8817c75dac8e6a973bd318ac0636c
João Rezende <joaozinhobent@gmail.com>
2018-07-19 00:28:19 -0300

    Implement main.asm (not yet functional)

--- main.asm
+++ main.asm
@@ -10,7 +10,25 @@
 
 .text
 main:
-	
-	# Stub.
-	jal plot_polynomial
-	nop
+	# Allocate space to store a polynomial as a string; store a pointer to that space in $s0.
+		li $v0, 9
+		li $a0, 200
+		syscall
+		move $s0, $v0
+	# Read a polynomial from the user.
+		li $v0, 8
+		move $a0, $s0
+		li $a1, 200
+		syscall
+	# Get an array of that polynomial's coefficients; store its address in $s1.
+		move $a0, $s0
+		jal read_polynomial
+		nop
+		move $s1, $v0
+	# Draw the polynomial's graph.
+		move $a0, $s1
+		jal plot_polynomial
+		nop
+	# Let the user do it again.
+		j main
+		nop
