6da9163b72f2a7d8a916e9a7e0ba2ed9c1bcf071
João Rezende <joaozinhobent@gmail.com>
2018-07-19 19:59:42 -0300

    Simplify plot_polynomial.asm with macros

--- plot_polynomial.asm
+++ plot_polynomial.asm
@@ -1,8 +1,44 @@
-.globl	plot_polynomial
-
 .include "global_constants.asm"
 
-plot_polynomial:
+.macro save_registers
+	add $sp, $sp, -4
+	sw $t0, ($sp)
+	add $sp, $sp, -4
+	sw $t1, ($sp)
+	add $sp, $sp, -4
+	sw $t3, ($sp)
+	add $sp, $sp, -4
+	sw $t4, ($sp)
+	add $sp, $sp, -4
+	sw $t5, ($sp)
+	add $sp, $sp, -4
+	sw $t6, ($sp)
+	add $sp, $sp, -4
+	sw $t7, ($sp)
+	add $sp, $sp, -4
+	sw $t8, ($sp)
+.end_macro
+
+.macro restore_registers
+	lw $t8, ($sp)
+	add $sp, $sp, 4
+	lw $t7, ($sp)
+	add $sp, $sp, 4
+	lw $t6, ($sp)
+	add $sp, $sp, 4
+	lw $t5, ($sp)
+	add $sp, $sp, 4
+	lw $t4, ($sp)
+	add $sp, $sp, 4
+	lw $t3, ($sp)
+	add $sp, $sp, 4
+	lw $t1, ($sp)
+	add $sp, $sp, 4
+	lw $t0, ($sp)
+	add $sp, $sp, 4
+.end_macro
+
+plot_polynomial:	.globl	plot_polynomial
 # Function that draws the graph of a polynomial (as evaluated by eval_polynomial) on the screen.
 # Argument:
 #	$a0: address of the integer array that represents the polynomial, to pass to eval_polynomial.
@@ -12,8 +48,8 @@ plot_polynomial:
 #	$t3: previous x.
 #	$t4: previous f(x).
 #	$t5: x coordinate of the last visible column (to be used as a stop condition).
-#	$t6: y coordinate of the highest visible column.
-#	$t7: y coordinate of the lowest visible column.
+#	$t6: y coordinate of the highest visible row.
+#	$t7: y coordinate of the lowest visible row.
 #	$t8: address of the integer array that represents the polynomial, to pass to eval_polynomial.
 
 	# Save $ra.
@@ -43,94 +79,27 @@ plot_polynomial:
 	# Draw the graph
 	for_every_x:
 		# Calculate f(x).
-			# Save $t0, $t1, $t3, $t4, $t5, $t6, $t7, $t8.
-				add $sp, $sp, -4
-				sw $t0, ($sp)
-				add $sp, $sp, -4
-				sw $t1, ($sp)
-				add $sp, $sp, -4
-				sw $t3, ($sp)
-				add $sp, $sp, -4
-				sw $t4, ($sp)
-				add $sp, $sp, -4
-				sw $t5, ($sp)
-				add $sp, $sp, -4
-				sw $t6, ($sp)
-				add $sp, $sp, -4
-				sw $t7, ($sp)
-				add $sp, $sp, -4
-				sw $t8, ($sp)
-			# Call eval_polynomial.
-				move $a0, $t0
-				move $a1, $t8
-				jal eval_polynomial
-				nop
-			# Recover $t0, $t1, $t3, $t4, $t5, $t6, $t7, $t8.
-				lw $t8, ($sp)
-				add $sp, $sp, 4
-				lw $t7, ($sp)
-				add $sp, $sp, 4
-				lw $t6, ($sp)
-				add $sp, $sp, 4
-				lw $t5, ($sp)
-				add $sp, $sp, 4
-				lw $t4, ($sp)
-				add $sp, $sp, 4
-				lw $t3, ($sp)
-				add $sp, $sp, 4
-				lw $t1, ($sp)
-				add $sp, $sp, 4
-				lw $t0, ($sp)
-				add $sp, $sp, 4
-			# Store eval_polynomial's return value in $t1.
-				move $t1, $v0
+			move $a0, $t0
+			move $a1, $t8
+			save_registers
+			jal eval_polynomial
+			nop
+			restore_registers
+			move $t1, $v0
 		# Check whether the display row of index f(x) is visible in the graph window. If it isn't, don't try to paint anything.
 			bgt $t1, $t6, update_t3_t4
 			nop
 			blt $t1, $t7, update_t3_t4
 			nop
 		# Paint the correspoding point in the plane, and draw a line between it and the previous point.
-			# Save $t0, $t1, $t3, $t4, $t5, $t6, $t7, $t8.
-				add $sp, $sp, -4
-				sw $t0, ($sp)
-				add $sp, $sp, -4
-				sw $t1, ($sp)
-				add $sp, $sp, -4
-				sw $t3, ($sp)
-				add $sp, $sp, -4
-				sw $t4, ($sp)
-				add $sp, $sp, -4
-				sw $t5, ($sp)
-				add $sp, $sp, -4
-				sw $t6, ($sp)
-				add $sp, $sp, -4
-				sw $t7, ($sp)
-				add $sp, $sp, -4
-				sw $t8, ($sp)
-			# Call draw_line.
-				move $a0, $t3
-				move $a1, $t4
-				move $a2, $t0
-				move $a3, $t1
-				jal draw_line
-				nop
-			# Recover $t0, $t1, $t3, $t4, $t5, $t6, $t7, $t8.
-				lw $t8, ($sp)
-				add $sp, $sp, 4	
-				lw $t7, ($sp)
-				add $sp, $sp, 4	
-				lw $t6, ($sp)
-				add $sp, $sp, 4	
-				lw $t5, ($sp)
-				add $sp, $sp, 4				
-				lw $t4, ($sp)
-				add $sp, $sp, 4
-				lw $t3, ($sp)
-				add $sp, $sp, 4
-				lw $t1, ($sp)
-				add $sp, $sp, 4
-				lw $t0, ($sp)
-				add $sp, $sp, 4
+			move $a0, $t3
+			move $a1, $t4
+			move $a2, $t0
+			move $a3, $t1
+			save_registers
+			jal draw_line
+			nop
+			restore_registers
 		# Update $t3 and $t4.
 		update_t3_t4:
 			move $t3, $t0
